02504b5a06d0400837f8c009b385ca31f7126727
apple <1396539283@qq.com>
2017-02-08 12:43:19 +0800

    照片选择器

---
+++ read.js
@@ -0,0 +1,4 @@
+本工程是上传多张图片选择器的demo,适用于评价或报错或者动态.仿照微信朋友圈发布动态.最多选择9张.不过可以修改在工程中查找Lib--->MImgpicker--->MShowGroupAllSet --->//设置可添加图片最多个数!!!
+#define kMaxImageCount 9
+代码借鉴了他人的写法,如有雷同纯属巧合。
+地址:http://code.cocoachina.com/view/133981
